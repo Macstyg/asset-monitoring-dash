da7ec5b1f7656b38f863fae524ddf7ae68b1c84e
Macstyg <macstyg@gmail.com>
2026-06-04 22:20:16 +0100

    fix script

--- bin/render-start.sh
+++ bin/render-start.sh
@@ -2,6 +2,12 @@
 
 set -o errexit
 
-_build/prod/rel/asset_monitoring_dash/bin/asset_monitoring_dash eval "AssetMonitoringDash.Release.migrate_and_seed()"
+release="_build/prod/rel/asset_monitoring_dash/bin/asset_monitoring_dash"
+
+"$release" eval "AssetMonitoringDash.Release.migrate()"
+
+(
+  "$release" eval "AssetMonitoringDash.Release.seed()"
+) &
 
 exec _build/prod/rel/asset_monitoring_dash/bin/server
